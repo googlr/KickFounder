94f36f7a077d84bbcd825a02762f94fd36fd3867
googlr <googlr@users.noreply.github.com>
2017-04-13 20:06:41 -0400

    Create part1_dataset.sql

---
+++ part1_dataset.sql
@@ -0,0 +1,6 @@
+INSERT INTO `USER` VALUES('BobInBrooklyn','Bob Stark','qwert123','Change the world','Brooklyn','Skiing','8888 8888 8888 8888');
+INSERT INTO `USER` VALUES('johnth1@gmail.com','John Wu','12345678','To be or not to be','Manhattan','Money','2222 2222 2222 2222');
+INSERT INTO `USER` VALUES('AllenShow','Allen Degenerous','hi5allen','Gay pride, LGBT go','Queens','TV shows','7777 7777 7777 7777');
+INSERT INTO `USER` VALUES('CaptainAmerican','Steve Rogers','lovepeggy','I can do this all day','Brooklyn','Dancing','6666 6666 6666 6666');
+INSERT INTO `USER` VALUES('DeadPool','Wade Wilson','deadpool','Screw the heros','Canada','Porn','3333 3333 3333 3333');
+
