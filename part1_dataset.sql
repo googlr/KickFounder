fb6923ddfd9b2a245a5515e71fc2b398373d5ce0
googlr <googlr@users.noreply.github.com>
2017-04-13 20:16:47 -0400

    Update part1_dataset.sql
    add data of TABLE TAG

--- part1_dataset.sql
+++ part1_dataset.sql
@@ -1,9 +1,9 @@
+# USER
 INSERT INTO `USER` VALUES('BobInBrooklyn','Bob Stark','qwert123','Change the world','Brooklyn','Skiing','8888 8888 8888 8888');
 INSERT INTO `USER` VALUES('johnth1@gmail.com','John Wu','12345678','To be or not to be','Manhattan','Money','2222 2222 2222 2222');
 INSERT INTO `USER` VALUES('AllenShow','Allen Degenerous','hi5allen','Gay pride, LGBT go','Queens','TV shows','7777 7777 7777 7777');
 INSERT INTO `USER` VALUES('CaptainAmerican','Steve Rogers','lovepeggy','I can do this all day','Brooklyn','Dancing','6666 6666 6666 6666');
 INSERT INTO `USER` VALUES('DeadPool','Wade Wilson','deadpool','Screw the heros','Canada','Porn','3333 3333 3333 3333');
-
 
 # PROJECT
 INSERT INTO `PROJECT`(`projectname`, `loginname`, `description`, `projectstatus`, `minfund`, `maxfund`, `posttime`, `endtime`, `plantime`) VALUES ('Great song', 'johnth1@gmail.com', 'I want produce a jazz, do you like that?', 'ongoing', 200, 300, '2017-03-12 12:10:29', '2017-05-12 12:10:29', '2017-09-13 12:10:29');
@@ -11,3 +11,16 @@ INSERT INTO `PROJECT`(`projectname`, `loginname`, `description`, `projectstatus`
 INSERT INTO `PROJECT`(`projectname`, `loginname`, `description`, `projectstatus`, `minfund`, `maxfund`, `posttime`, `endtime`, `plantime`) VALUES ('Music part1', 'BobInBrooklyn', 'Jazz is my life! Come on, let dance.', 'ongoing', 340, 440, '2017-03-13 12:10:29', '2017-05-13 12:10:29', '2017-10-14 12:10:29');
 INSERT INTO `PROJECT`(`projectname`, `loginname`, `description`, `projectstatus`, `minfund`, `maxfund`, `posttime`, `endtime`, `plantime`) VALUES ('Music part2', 'BobInBrooklyn', 'Jazz is my life! Come on, let dance.', 'ongoing', 400, 500, '2017-03-14 12:10:29', '2017-05-14 12:10:29', '2017-10-15 12:10:29');
 INSERT INTO `PROJECT`(`projectname`, `loginname`, `description`, `projectstatus`, `minfund`, `maxfund`, `posttime`, `endtime`, `plantime`) VALUES ('Music part3', 'BobInBrooklyn', 'Jazz is my life! Come on, let dance.', 'ongoing', 500, 600, '2017-04-15 12:10:29', '2017-05-15 12:10:29', '2017-10-16 12:10:29');
+
+# TAG
+INSERT INTO `TAG` VALUES('Great song','jazz');
+INSERT INTO `TAG` VALUES('Great song','vocal');
+INSERT INTO `TAG` VALUES('Great song','music');
+INSERT INTO `TAG` VALUES('Good Music','jazz');
+INSERT INTO `TAG` VALUES('Good Music','music');
+INSERT INTO `TAG` VALUES('Music part1','jazz');
+INSERT INTO `TAG` VALUES('Music part1','music');
+INSERT INTO `TAG` VALUES('Music part2','jazz');
+INSERT INTO `TAG` VALUES('Music part2','music');
+INSERT INTO `TAG` VALUES('Music part3','jazz');
+INSERT INTO `TAG` VALUES('Music part3','music');
