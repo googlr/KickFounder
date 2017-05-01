9d853cff0732bb90816ee226e58aa9a782f4d6ce
googlr <xg626@gmail.com>
2017-05-01 13:05:39 -0400

    add newproject.php

---
+++ php/newproject.php
@@ -0,0 +1,32 @@
+<?php 
+session_start();
+//echo "SESSION START!"; 
+if( ! isset($_SESSION['loginname']) )
+  $_SESSION['loginname'] = $_POST["loginname"];
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+Welcome <?php echo $_POST["loginname"]; ?><br>
+Start your new project here.<br><br />
+
+<form action="createproject.php" method="post">
+ <fieldset>
+  <legend>Project information:</legend>
+    <input type="hidden" name="loginname" value="<?php echo $_SESSION['loginname']; ?>">
+    Project Name: <input type="text" name="projectname" required><br>
+    Description: <input type="textaera" name="description" required><br>
+    Expected Fund:<br>
+    Minimum: <input type="number" name="minfund" min=1>
+    Maximum: <input type="number" name="maxfund" min=1> <br>
+    Time Schedule:<br>
+    Pledge Endtime: <input type="date" name="pledgetime"><br>
+    Project Endtime: <input type="date" name="plantime"><br>
+ </fieldset>
+<input type="submit" value="Submit Project">
+</form>
+
+</body>
+</html>
