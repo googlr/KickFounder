6ac7575834a908595a791d748f565ae76fe51726
WLSK <cw2661@nyu.edu>
2017-05-04 13:59:18 -0400

    Add files via upload

--- php/browse.php
+++ php/browse.php
@@ -12,51 +12,64 @@
 		$mysql_password="root"; // password
 		$mysql_database="kickfounder"; // database name
 		$con = new mysqli($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);
+		
 		if ($con->connect_error) {
 			die("Database connect_error: " . $con->connect_error);
 		}		
-		
+		echo "<p>Projects:</p>";
 		// Project list
 		if ($keyw == "" ) {
 				$project_sql = "SELECT * FROM PROJECT";
 				$useract_sql = "INSERT INTO `USERACT` VALUES('".$_SESSION['loginname']."',now(), 'search', 'ALL*')";
-			}
-			else {
-				//$project_sql = "SELECT * FROM PROJECT WHERE projectname LIKE "."\"%".$keyw."%\"";
+				$project_result = $con->query($project_sql);
+				if ($project_result->num_rows > 0) {
+			        while($row = $project_result->fetch_assoc()) {
+				        echo "<p><a href='displayproject.php?projectname=".$row["projectname"]."'>".$row["projectname"]."</a></p>";
+					}
+            }
+		
+		}
+		else {
+			//$project_sql = "SELECT * FROM PROJECT WHERE projectname LIKE "."\"%".$keyw."%\"";
 
-				/* Prepared statement, stage 1: prepare */
-				if (!($stmt = $con->prepare("SELECT * FROM PROJECT WHERE projectname LIKE ?"))) {
-    				echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
-				}
+			/* Prepared statement, stage 1: prepare */
+			if (!($stmt = $con->prepare("SELECT * FROM PROJECT WHERE projectname LIKE ?"))) {
+    			echo "Prepare failed: (" . $con->errno . ") " . $con->error;
+			}
 				//bind the variables to the stmt
 				$keywords = "%".$keyw."%";
 				$stmt -> bind_param("s",$keywords);
 				//execute
 				$stmt ->execute();
 				$project_result = $stmt->get_result();
+				
+				if ($project_result->num_rows > 0) {
+			
+			        while($row = $project_result->fetch_assoc()) {
+				        echo "<p><a href='displayproject.php?projectname=".$row["projectname"]."'>".$row["projectname"]."</a></p>";
+					}
+				}
+				$stmt -> close();
 
 
 				//$useract_sql = "INSERT INTO `USERACT` VALUES('".$_SESSION['loginname']."',now(), 'search', '".$keyw."')";
 				/* Prepared statement, stage 1: prepare */
-				if (!($ureract_stmt = $con->prepare("INSERT INTO `USERACT` VALUES('".$_SESSION['loginname']."',now(), 'search', ?"))) {
- 			   		echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+				if (!($ureract_stmt = $con->prepare("INSERT INTO `USERACT` VALUES('".$_SESSION['loginname']."',now(), 'search', ?)"))) {
+ 			   		echo "Prepare failed: (" . $con->errno . ") " . $con->error;
 				}
 
 				//bind the variables to the ureract_stmt
-				$ureract_stmt -> bind_param("s",$keywords);
+				$ureract_keyw = $keyw;
+				$ureract_stmt -> bind_param("s", $ureract_keyw );
 				//execute
 				$ureract_stmt ->execute();
 				
+				
+				
 			}
-		mysqli_query($con, $useract_sql);
-		//$project_result = $con->query($project_sql);
-		echo "<p>Projects:</p>";
-		if ($project_result->num_rows > 0) {
-			
-			while($row = $project_result->fetch_assoc()) {
-				echo "<p><a href='displayproject.php?projectname=".$row["projectname"]."'>".$row["projectname"]."</a></p>";
-			}
-		}
+
+		
+		
 		echo "<p><a href=\"./home.php\"><input type=\"button\" value=\"Back To Home\"></input></a></p>";
 		
 	?>
